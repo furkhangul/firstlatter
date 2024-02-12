0af148103d78016bfdfa1ba6099410ca1812c04b
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-02-12 09:32:42 +0300

    Create firstlatter.c

---
+++ firstlatter.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+      char isim;
+      printf("İsim :");
+      isim=getchar();
+      printf("\nAramiza Hoş geldin ");
+      putchar(isim);
+      return 0;
+}
+    
